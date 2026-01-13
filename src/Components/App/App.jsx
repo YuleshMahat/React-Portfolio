008d9b790f8dba40c650aeb92aef4d3993fe42ba
Yulesh Mahat <yuleshkazuto@gmail.com>
2026-01-13 15:37:19 +1100

    Add loading screen to the website and fix animation glitch

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar.jsx";
 import Intro from "../Intro/Intro.jsx";
 import styles from "./App.module.css";
 import Skills from "../Skills/Skills.jsx";
 import Projects from "../Projects/Projects.jsx";
 import Contact from "../Contact/Contact.jsx";
+import { fetchSkillsAction } from "@/features/skills/skillsActions.js";
 
 const App = () => {
+  const [skills, setSkills] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      const result = await fetchSkillsAction();
+      console.log(result);
+      if (result.status === "success") setSkills(result.data);
+      setLoading(false);
+    };
+    fetchSkills();
+  }, []);
+
   return (
     <div>
-      <div className={styles.container1}>
-        <NavBar />
-        <Intro />
-        <Skills />
-        <Projects />
-      </div>
-      <div className={styles.container2}>
-        <Contact />
-      </div>
+      {loading ? (
+        <div className={styles.loadingScreen}>
+          <h1>
+            Loading<span className={styles.dots}></span>
+          </h1>
+        </div>
+      ) : (
+        <div>
+          <div className={styles.container1}>
+            <NavBar />
+            <Intro />
+            <Skills skills={skills} />
+            <Projects />
+          </div>
+          <div className={styles.container2}>
+            <Contact />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
 export default App;
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,8 @@
 "use client";
 
 import React from "react";
-import NavBar from "../Components/NavBar/NavBar.jsx";
-import Intro from "../Components/Intro/Intro.jsx";
-import styles from "../Components/App/App.module.css";
-import Skills from "../Components/Skills/Skills.jsx";
-import Projects from "../Components/Projects/Projects.jsx";
-import Contact from "../Components/Contact/Contact.jsx";
+import App from "@/Components/App/App.jsx";
 
 export default function Home() {
-  return (
-    <div>
-      <div className={styles.container1}>
-        <NavBar />
-        <Intro />
-        <Skills />
-        <Projects />
-      </div>
-      <div className={styles.container2}>
-        <Contact />
-      </div>
-    </div>
-  );
+  return <App />;
 }
